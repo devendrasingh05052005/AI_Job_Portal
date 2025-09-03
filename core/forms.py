5fc6a584c54461718fc0ab7186e601aa49d6a127
devendra <banad8640@gmail.com>
2025-09-03 21:22:55 +0530

    Initial commit - ATS Project

---
+++ core/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser, Job, Application, JobField
+
+
+
+class CandidateSignUpForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = ('username', 'is_candidate')
+
+
+class JobPostingForm(forms.ModelForm):
+    job_field = forms.ModelChoiceField(queryset=JobField.objects.all(), empty_label="Select a job field")
+    
+    class Meta:
+        model = Job
+        fields = ['title', 'description', 'is_internal', 'external_url', 'job_field']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'external_url': forms.URLInput(attrs={'class': 'form-control'}),
+        }
+
+
+
+class ApplicationForm(forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = ['resume']
